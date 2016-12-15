456d113c34417931f7ab9d264fd34b601d5d1271
louisvno <louisvn@gmail.com>
2016-12-15 17:52:26 +0100

    Add Ship class and Ship repo

---
+++ src/main/java/edu/example/ShipRepository.java
@@ -0,0 +1,14 @@
+package edu.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by louis on 12/15/2016.
+ */
+
+@RepositoryRestResource
+public interface ShipRepository extends JpaRepository<Ship, Long>{
+
+}
+
