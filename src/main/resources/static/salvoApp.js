bc9d185a40d7d21fdafdfa7e4737ebea4829f6f9
louisvno <louisvn@gmail.com>
2016-12-15 15:55:44 +0100

    Add gamelist dynamic webpage with angular (end task 2.4)

---
+++ src/main/resources/static/salvoApp.js
@@ -0,0 +1,28 @@
+
+ //send an Ajax request to /games
+ //pass the data that is eventually returned to a function that creates LI elements for each game date
+ //and stores the list in the OL element on the web page. See the Resources for how to create a list from a list using map.
+
+ // pass http service to the controller
+function viewController($http, $scope) {
+  $scope.games;
+
+  $http.get("/api/games")
+     .then(function(response) {
+        tempGames = angular.fromJson(response.data);
+        tempGames.forEach (function(game){
+            game.created = getDate(game.created);
+        })
+        $scope.games = tempGames;
+     });
+}
+
+angular
+    .module('salvoGame', [])
+    .controller('viewController', viewController);
+
+//auxiliary functions
+function getDate(dateString){
+    date = new Date (dateString);
+    return date.toTimeString();
+}
