3658f095a10ff1d506ff11f6f749af09dd87d20e
louisvno <louisvn@gmail.com>
2016-12-19 14:15:40 +0100

    setup for jquery frontend

--- src/main/resources/static/salvoApp.js
+++ src/main/resources/static/salvoApp.js
@@ -4,25 +4,3 @@
  //and stores the list in the OL element on the web page. See the Resources for how to create a list from a list using map.
 
  // pass http service to the controller
-function viewController($http, $scope) {
-  $scope.games;
-
-  $http.get("/api/games")
-     .then(function(response) {
-        tempGames = response.data;
-        tempGames.forEach (function(game){
-            game.created = getDate(game.created);
-        })
-        $scope.games = tempGames;
-     });
-}
-
-angular
-    .module('salvoGame', [])
-    .controller('viewController', viewController);
-
-//auxiliary functions
-function getDate(dateString){
-    date = new Date (dateString);
-    return date.toTimeString();
-}
