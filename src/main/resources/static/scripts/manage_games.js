1fdc460b1ccbdb390c529f87dd80fdf57e795fb4
louisvno <louisvn@gmail.com>
2017-01-06 19:14:33 +0100

    add manage games page (mod2. task 1.6)

---
+++ src/main/resources/static/scripts/manage_games.js
@@ -0,0 +1,54 @@
+
+$(function(){
+    loadGames()
+  });
+
+function loadGames(){
+    $.ajax({ method:"GET",
+             url: "/api/games",
+             dataType: "json",
+             success: function (data) {
+                    renderGamesList(data.games);
+             },
+             error: function (){
+               $('#games-list').html('<span>Games list unavailable</span>');
+             }
+          });
+}
+
+function renderGamesList(games){
+    $('#games-list').html(addGames(games));
+}
+
+function addGames(games){
+   return games.reduce(function(str,game){
+      return  str +
+              "<div class='game-card'>" +
+                   "<ul>"+
+                      "<li>"+ game.id + "</li>" +
+                       addPlayers(game) +
+                      "<li>"+ game.created + "</li>" +
+                   "</ul>" +
+                   addJoinButton(game) +
+              "<div>";
+    },"");
+}
+
+function addJoinButton(game){
+    var ids = Object.keys(game.gamePlayers);
+    if (ids.length < 2 ){
+        return "<button>Join</button>";
+    } else
+    return "";
+}
+
+function addPlayers (game){
+    var str = "";
+      Object.keys(game.gamePlayers).forEach(key => {
+           str +=
+           "<li>" +
+             game.gamePlayers[key].player.firstName +
+           "</li>"
+      })
+    return str;
+}
